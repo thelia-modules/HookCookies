8880c6dc8a601f921f3f01ce91a11dbc6c53589b
lopes-vincent <vlopes@openstudio.fr>
2016-03-22 11:04:53 +0100

    Fix typo and thelia version

---
+++ I18n/frontOffice/default/fr_FR.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'Cookies help us provide our services and to offer you personalized experiences. By using this site, you agree to our use of cookies. You can change your preferences at any time.' => 'Les cookies nous aident à vous proposer ce service ainsi qu\'une expérience personnalisée. En naviguant sur ce site, vous agréez l\'utilisation de nos cookies.',
+
+);
--- i18n/frontOffice/default/fr_FR.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'Cookies help us provide our services and to offer you personalized experiences. By using this site, you agree to our use of cookies. You can change your preferences at any time.' => 'Les cookies nous aident à vous proposer ce service ainsi qu\'une expérience personalisée. En naviguant sur ce site, vous agréez l\'utilisation de nos cookies.',
-
-);
