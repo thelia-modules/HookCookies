5009170db234bb6a66008edeab9daf8f4177644d
vz777 <vincentbourbon@hotmail.fr>
2023-01-20 19:03:59 +0100

    Ceci est mon premier vrai commit. Branche main et modifications pour compatibilité 2.5

--- HookCookies.php
+++ HookCookies.php
@@ -12,9 +12,19 @@
 
 namespace HookCookies;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Thelia\Module\BaseModule;
+
 
 class HookCookies extends BaseModule
 {
+
+    public static function configureServices(ServicesConfigurator $servicesConfigurator): void
+    {
+        $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
+            ->exclude([THELIA_MODULE_DIR.ucfirst(self::getModuleCode()).'/I18n/*'])
+            ->autowire(true)
+            ->autoconfigure(true);
+    }
 
 }
